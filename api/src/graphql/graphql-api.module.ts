d8b6e0ed3edc79e63f51ac51b9ecd02952f9451a
Alexander Openkowski <alexander.openkowski@the-nu-company.com>
2022-02-19 10:22:36 +0100

    Try `ApolloDriverAsyncConfig` version 1

--- api/src/graphql/graphql-api.module.ts
+++ api/src/graphql/graphql-api.module.ts
@@ -2,21 +2,23 @@ import { Module } from "@nestjs/common"
 import { GraphQLModule } from "@nestjs/graphql"
 import { AppConfigModule } from "../config/app-config.module"
 import { AppConfigService } from "../config/app-config.service"
-import { join } from "path"
-import {ApolloDriver, ApolloDriverConfig} from "@nestjs/apollo";
+import {ApolloDriver, ApolloDriverAsyncConfig} from "@nestjs/apollo";
 
 @Module({
 	imports: [
-		GraphQLModule.forRootAsync<ApolloDriverConfig>({
+		GraphQLModule.forRootAsync<ApolloDriverAsyncConfig>({
 			driver: ApolloDriver,
 			imports: [AppConfigModule],
-			inject: [AppConfigService],
-			useFactory: (appConfig: AppConfigService) => ({
-				debug: appConfig.get.graphql.debug,
-				playground: appConfig.get.graphql.playground,
-				autoSchemaFile: join(process.cwd(), "src/graphql/schema.gql"),
-				cors: appConfig.get.cors,
+			useFactory: async (configService: AppConfigService) => ({
+				// ^-- example from docs does not work.
+				typePaths: "some_path",
+				// TODO: How to configure other options below?
+				//  debug: appConfig.get.graphql.debug,
+				//  playground: appConfig.get.graphql.playground,
+				//  autoSchemaFile: join(process.cwd(), "src/graphql/schema.gql"),
+				//  cors: appConfig.get.cors,
 			}),
+			inject: [AppConfigService],
 		}),
 	],
 })
